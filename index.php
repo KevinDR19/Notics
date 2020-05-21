a2a49cbb15dddaf5e047bab185b8cffafe8ee2ac
KevinDR19 <kevin26rg@hotmail.com>
2020-05-21 13:34:39 -0500

    Hacemos referencia a archivos css y js de bootstrap 4

--- index.php
+++ index.php
@@ -5,32 +5,46 @@
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Login</title>
+
+    <link rel="stylesheet" href="Librerias/bootstrap-4.4.1/css/bootstrap.css">
 
 </head>
 
 <body>
 
     <div class="container">
+
         <form action="" method="post">
+
             <label for="nombre">Nombre:</label>
+
             <input type="text" name="nombre" id="nombre">
 
             <label for="apellido">Apellido:</label>
+
             <input type="text" name="apellido" id="apellido">
 
             <label for="nombreUsuario">Nombre de usuario:</label>
+
             <input type="text" name="nombreUsuario" id="nombreUsuario">
 
             <label for="email">Email:</label>
+
             <input type="email" name="email" id="email">
 
             <label for="contrasenia">Contraseña:</label>
+
             <input type="password" name="contrasenia" id="contrasenia">
 
             <button type="submit">Enviar</button>
+
         </form>
+
     </div>
+
+    <script src="Librerias/bootstrap-4.4.1/js/bootstrap.js"></script>
 
 </body>
 
