5fdfe04ec6c9bf0f22036b6da4448ff75b8c4129
KevinDR19 <kevin26rg@hotmail.com>
2020-05-21 13:12:25 -0500

    Creamos campos del form y el botón enviar

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-
 <html lang="es">
 
 <head>
@@ -13,12 +12,26 @@
 <body>
 
     <div class="container">
-
         <form action="" method="post">
-            
-        </form>
+            <label for="nombre">Nombre:</label>
+            <input type="text" name="nombre" id="nombre">
 
+            <label for="apellido">Apellido:</label>
+            <input type="text" name="apellido" id="apellido">
+
+            <label for="nombreUsuario">Nombre de usuario:</label>
+            <input type="text" name="nombreUsuario" id="nombreUsuario">
+
+            <label for="email">Email:</label>
+            <input type="email" name="email" id="email">
+
+            <label for="contrasenia">Contraseña:</label>
+            <input type="password" name="contrasenia" id="contrasenia">
+
+            <button type="submit">Enviar</button>
+        </form>
     </div>
+
 </body>
 
 </html>
