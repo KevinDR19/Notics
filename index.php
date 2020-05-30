fc0e34e79a46b0ffcebbf4e7fed64ab7826eb924
KevinDR19 <kevin26rg@hotmail.com>
2020-05-29 19:44:48 -0500

    Cambiamos el contenido de la etiqueta title

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Login</title>
+    <title>Registro de usuario</title>
 
     <link rel="stylesheet" href="Librerias/bootstrap-4.4.1/css/bootstrap.css">
 
