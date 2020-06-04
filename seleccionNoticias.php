782e02e97ae99b1e2ed1d8d1e961aa2222434343
KevinDR19 <kevin26rg@hotmail.com>
2020-06-04 11:53:55 -0500

    Hacemos referencia a varias librerías

--- seleccionNoticias.php
+++ seleccionNoticias.php
@@ -1,11 +1,32 @@
 <!DOCTYPE html>
+
 <html lang="es">
+
 <head>
+    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <title>Seleccionar noticia</title>
+
+    <link rel="stylesheet" href="Librerias/bootstrap-4.4.1/css/bootstrap.css">
+
+    <link rel="stylesheet" href="Librerias/fontawesome-5.13.0/css/all.css">
+
+    <link rel="stylesheet" href="Librerias/sweetalert2/sweetalert/sweetalert2.min.css">
+
 </head>
+
 <body>
     
+    
+
+    <script src="Librerias/jquery-3.4.1/jquery-3.4.1.js"></script>
+
+    <script src="Librerias/bootstrap-4.4.1/js/bootstrap.js"></script>
+
+    <script src="Librerias/sweetalert2/sweetalert/sweetalert2.all.min.js"></script>
+
 </body>
+
 </html>
