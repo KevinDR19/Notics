c826179359e7d45cdcf07777b58ffde8d19ac82e
KevinDR19 <kevin26rg@hotmail.com>
2020-06-04 12:37:59 -0500

    Hacemos referencia a varias librerías

--- noticias.php
+++ noticias.php
@@ -15,12 +15,24 @@
     
     <title>Noticias</title>
 
+    <link rel="stylesheet" href="Librerias/bootstrap-4.4.1/css/bootstrap.css">
 
+    <link rel="stylesheet" href="Librerias/fontawesome-5.13.0/css/all.css">
+
+    <link rel="stylesheet" href="Librerias/sweetalert2/sweetalert/sweetalert2.min.css">
 
 </head>
 
 <body>
     
+    
+
+    <script src="Librerias/jquery-3.4.1/jquery-3.4.1.js"></script>
+
+    <script src="Librerias/bootstrap-4.4.1/js/bootstrap.js"></script>
+
+    <script src="Librerias/sweetalert2/sweetalert/sweetalert2.all.min.js"></script>
+
 </body>
 
 </html>
